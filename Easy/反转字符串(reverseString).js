97c99d9e9e610d6af3b185fc46afbf0af65f05ae
qiuyang0214 <qiuyang0214@outlook.com>
2019-04-30 17:28:55 +0800

    esay > reverseString

---
+++ Easy/反转字符串(reverseString).js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ * @description 执行用时 : 212 ms, 在Reverse String的JavaScript提交中击败了66.05% 的用户
+ *              内存消耗 : 46.5 MB, 在Reverse String的JavaScript提交中击败了79.22% 的用户
+ */
+var reverseString = function(s) {
+    return s.reverse()
+};
